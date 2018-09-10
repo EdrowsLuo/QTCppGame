eb1ff6ca6e408b00e81fa61cadbabcbe90bd26a0
hhhooo <22672273522@qq.com>
2018-09-10 22:38:38 +0800

    。。。。

--- tracke.h
+++ tracke.h
@@ -1,12 +1,14 @@
 #ifndef TRACKE_H
 #define TRACKE_H
 #include "MyDraw.h"
-
+#include "defext.h"
 class trackE : public MyDraw
 {
 public:
     trackE(int);
     void draw(QPaintEvent *event,QPainter *painter);
+    //GetSet(int,judge)
+private:
     int judge;
 };
 
